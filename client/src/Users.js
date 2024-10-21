f89414a66345bcea31d26cb04734ce74ec81cd7a
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-10-21 15:00:41 +0530

    Add table component to users

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import UserForm from './UserForm'
+import UsersTable from './UsersTable'
+import { Box } from '@mui/material'
+
+const users = [
+    {
+        id: 1,
+        name: 'Leshan'
+    },
+    {
+        id: 2,
+        name: 'Sanjeewa'
+    }
+];
 
 const Users = () => {
   return (
-    <UserForm/>
+    <Box
+        xs={{
+            width: 'calc(100% - 100px)',
+            margin: 'auto',
+            marginTop: '100px',
+        }}
+    >    
+        <UserForm/>
+        <UsersTable rows={users}/>
+    </Box>
   )
 }
 
